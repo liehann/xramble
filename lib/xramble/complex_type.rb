42c74463a267ebbca994e7f4edd5bf99f17b5afe
liehann <liehannl@gmail.com>
2010-06-19 15:52:42 +0200

    Added generation of accessor methods for element and attribute module methods.

--- lib/xramble/complex_type.rb
+++ lib/xramble/complex_type.rb
@@ -17,10 +17,42 @@ module Xramble
       def namespace(url, options = {})
       end
 
+      # Defines an instance variable that is marshalled as an element.
+      #  class MyDoc
+      #    include Xramble::ComplexType
+      #    element :element
+      #  end
+      # will get marshalled as
+      #  <MyDoc>
+      #    <element></element>
+      #  </MyDoc>
       def element(name, options = {})
+        add_accessor_methods(name)
       end
 
+      # Defines an instance variable that is marshalled as an attribute.
+      #  class MyDoc
+      #    include Xramble::ComplexType
+      #    attribute :attribute
+      #  end
+      # will get marshalled as
+      #  <MyDoc attribute=''>
+      #  </MyDoc>
       def attribute(name, options = {})
+        add_accessor_methods(name)
+      end
+
+      private
+
+      def add_accessor_methods(name)
+        inst_symbol = "@#{name}".to_sym
+        define_method("#{name}=") do |value|
+          self.instance_variable_set(inst_symbol, value)
+        end
+
+        define_method(name) do
+          self.instance_variable_get(inst_symbol)
+        end
       end
     end
   end
--- test/test_complex_type.rb
+++ test/test_complex_type.rb
@@ -14,6 +14,32 @@ class TestComplexType < Test::Unit::TestCase
       assert(CT.methods.include?('namespace'))
     end
 
+    should "define accessor methods for attributes" do
+      class CT
+        include Xramble::ComplexType
+        attribute :name
+      end
+
+      ct = CT.new()
+      assert(ct.methods.include?('name'))
+      assert(ct.methods.include?('name='))
+      ct.name = 'value'
+      assert_equal('value', ct.name)
+    end
+
+    should "define accessor methods for elements" do
+      class CT
+        include Xramble::ComplexType
+        element :element
+      end
+
+      ct = CT.new()
+      assert(ct.methods.include?('element'))
+      assert(ct.methods.include?('element='))
+      ct.element = 'value'
+      assert_equal('value', ct.element)
+    end
+
   end
 end
 
