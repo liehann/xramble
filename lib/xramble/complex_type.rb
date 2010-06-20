f3a6e504029bc2f455ad93206d17653f903ef13a
liehann <liehannl@gmail.com>
2010-06-20 23:22:29 +0200

    Added namespace method, attributes and elements methods and accessors.

--- lib/xramble/complex_type.rb
+++ lib/xramble/complex_type.rb
@@ -6,15 +6,73 @@ module Xramble
   #    include Xramble::ComplexType
   #    
   #    attribute :name
-  #    element :name
+  #    element :node
   #  end
   module ComplexType
     def self.included(mod)
       mod.extend ClassMethods
+      mod.send(:include, InstanceMethods)
+
+      # Initialize class variables
+      mod.send(:class_variable_set, :@@namespace, nil)
+      mod.send(:class_variable_set, :@@elements, [])
+      mod.send(:class_variable_set, :@@attributes, [])
+    end
+
+    module InstanceMethods
+
+      # Returns and xml string representation of the object.
+      #
+      # Example:
+      #  class MyComplexType
+      #    include Xramble::ComplexType
+      #    
+      #    attribute :name
+      #    element :node
+      #  end
+      #  
+      #  obj = MyComplexType.new()
+      #  obj.to_xml
+      #  >>> <MyComplexType name=''><node/></MyComplexType>
+      def to_xml(namespaces = {})
+      end
+
+      def attributes
+        self.class.attributes
+      end
+
+      def elements
+        self.class.elements
+      end
+
     end
 
     module ClassMethods
-      def namespace(url, options = {})
+      # Set or retrive the object xml namespace.
+      #
+      #  class MyComplexType
+      #    include Xramble::ComplexType
+      #    namespace 'namespace:uri'
+      #  end
+      #  
+      #  MyComplexType.namespace
+      #  >>> 'namespace:uri'
+      #
+      # A namespace prefix can be supplied.
+      #  class MyComplexType
+      #    include Xramble::ComplexType
+      #    namespace 'namespace:uri', 'ns'
+      #  end
+      #  
+      #  MyComplexType.namespace_prefix
+      #  >>> 'ns'
+      def namespace(*args)
+        if (args.size > 0)
+          @@namespace = args[0]
+          @@namespace_prefix = args[1]
+        else
+          @@namespace
+        end
       end
 
       # Defines an instance variable that is marshalled as an element.
@@ -27,6 +85,7 @@ module Xramble
       #    <element></element>
       #  </MyDoc>
       def element(name, options = {})
+        elements << name
         add_accessor_methods(name)
       end
 
@@ -39,7 +98,16 @@ module Xramble
       #  <MyDoc attribute=''>
       #  </MyDoc>
       def attribute(name, options = {})
+        attributes << name
         add_accessor_methods(name)
+      end
+
+      def attributes
+        class_variable_get(:@@attributes)
+      end
+
+      def elements
+        class_variable_get(:@@elements)
       end
 
       private
--- test/test_complex_type.rb
+++ test/test_complex_type.rb
@@ -1,14 +1,13 @@
 require 'test_helper'
 
 class TestComplexType < Test::Unit::TestCase
-  context "the ComplexType module" do
+  context "including the ComplexType module" do
 
     should "add module methods" do
       class CT
         include Xramble::ComplexType
       end
 
-      # puts (CT.methods - Object.methods).inspect
       assert(CT.methods.include?('element'))
       assert(CT.methods.include?('attribute'))
       assert(CT.methods.include?('namespace'))
@@ -19,6 +18,8 @@ class TestComplexType < Test::Unit::TestCase
         include Xramble::ComplexType
         attribute :name
       end
+
+      assert(CT.attributes.include?(:name))
 
       ct = CT.new()
       assert(ct.methods.include?('name'))
@@ -33,11 +34,32 @@ class TestComplexType < Test::Unit::TestCase
         element :element
       end
 
+      assert(CT.elements.include?(:element))
+
       ct = CT.new()
       assert(ct.methods.include?('element'))
       assert(ct.methods.include?('element='))
       ct.element = 'value'
       assert_equal('value', ct.element)
+    end
+
+    should "define a to_xml method" do
+      class CT
+        include Xramble::ComplexType
+      end
+
+      ct = CT.new()
+      assert(ct.methods.include?('to_xml'))
+    end
+
+    should "define a namespace method" do
+      class CT
+        include Xramble::ComplexType
+        namespace 'namespace:uri'
+      end
+
+      assert(CT.methods.include?('namespace'))
+      assert_equal('namespace:uri', CT.namespace)
     end
 
   end
